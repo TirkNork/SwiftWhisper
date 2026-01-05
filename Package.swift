dcd3947b24b8c69cc007f58928dc60aafa3d8299
Kornkrit R <kornkrit.r@kasikornline.com>
2026-01-05 17:33:30 +0700

    use coreml

--- Package.swift
+++ Package.swift
@@ -19,6 +19,8 @@ let package = Package(
                 exclude: exclude,
                 cSettings: [
                     .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .define("WHISPER_COREML_ALLOW_FALLBACK", .when(platforms: [.macOS, .macCatalyst, .iOS]))
                 ]),
         .testTarget(name: "WhisperTests", dependencies: [.target(name: "SwiftWhisper")], resources: [.copy("TestResources/")])
     ],
